97f6152fb847396675e780b616975685672e228d
41023226 <41023226@gm.nfu.edu.tw>
2023-03-02 16:21:22 +0800

    add website

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site \n 2b-pj1bg15', 'tags': '', 'url': 'About.html'}]};
