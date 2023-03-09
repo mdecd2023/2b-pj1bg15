4c7d5edcba3f833a48f2446cbf4b4fac6e2d591d
41023226 <41023226@gm.nfu.edu.tw>
2023-03-09 13:46:39 +0800

    add website

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site \n 2b-pj1bg15', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': '倉儲: https://github.com/mdecd2023/2b-pj1bg15 \n 網站: https://mdecd2023.github.io/2b-pj1bg15', 'tags': '', 'url': 'About.html'}]};
